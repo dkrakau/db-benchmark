10f89bde5c1625ef1abb802dcd087d0ec088ccfc
Dominik Krakau <dominik.krakau@outlook.de>
2024-09-09 14:25:50 +0200

    Add functionality to milvus provider

--- backend/src/provider/milvus.provider.ts
+++ backend/src/provider/milvus.provider.ts
@@ -1,5 +1,12 @@
 import { Injectable } from "@nestjs/common";
-import { GetVersionResponse, MilvusClient } from "@zilliz/milvus2-sdk-node";
+import { CreateIndexParam,
+    DescribeCollectionResponse,
+    FieldType,
+    GetVersionResponse,
+    MilvusClient,
+    ResStatus,
+    ShowCollectionsResponse,
+    ShowPartitionsResponse } from "@zilliz/milvus2-sdk-node";
 
 @Injectable()
 export class MilvusProvider {
@@ -10,7 +17,91 @@ export class MilvusProvider {
         this.client = new MilvusClient("http://localhost:19530");
     }
 
+    getClient(): MilvusClient {
+        return this.client;
+    }
+    
+    async createCollection(
+        collectionName: string,
+        description: string,
+        dim: number,
+        metricType: string,
+        consistencyLevel: 'Strong' | 'Session' | 'Bounded' | 'Eventually' | 'Customized',
+        indexParams: CreateIndexParam,
+        fields: FieldType[],
+        autoId: boolean): Promise<ResStatus> {
+
+        const response = await this.client.createCollection({
+            collection_name: collectionName,
+            description: description,
+            dimension: dim,
+            metric_type: metricType,
+            consistency_level: consistencyLevel,
+            index_params: indexParams,
+            fields: fields,
+            auto_id: autoId
+            });
+
+        return response;
+    }
+
+    async dropCollection(collectionName: string): Promise<ResStatus> {
+        const response = await this.client.dropCollection({
+            collection_name: collectionName
+        });
+        return response;
+    }
+
+    async createPartition(collectionName: string, partitionName: string): Promise<ResStatus> {
+        const response = await this.client.createPartition({
+            collection_name: collectionName,
+            partition_name: partitionName
+        });
+        return response;
+    }
+
+    async dropPartition(collectionName: string, partitionName: string): Promise<ResStatus> {
+        const response = await this.client.dropPartition({
+            collection_name: collectionName,
+            partition_name: partitionName
+        });
+        return response;
+    }
+
+    async loadCollection(collectionName: string): Promise<ResStatus> {
+        const response = await this.client.loadCollection({
+            collection_name: collectionName
+        });
+        return response;
+    }
+
+    async releaseCollection(collectionName: string): Promise<ResStatus> {
+        const response = await this.client.releaseCollection({
+            collection_name: collectionName
+        });
+        return response;
+    }
+
+    async listCollections(): Promise<ShowCollectionsResponse> {
+       return await this.client.listCollections();
+    }
+
+    async describeColleciton(collectionName: string): Promise<DescribeCollectionResponse> {
+        const response = await this.client.describeCollection({
+            collection_name: collectionName
+        });
+        return response;
+    }
+
+    async listPartitionsInCollection(collectionName: string): Promise<ShowPartitionsResponse> {
+        const response = await this.client.listPartitions({
+            collection_name: collectionName
+        })
+        return response;
+    }
+
     async getVersion(): Promise<GetVersionResponse> {
         return await this.client.getVersion();
     }
+    
 }
