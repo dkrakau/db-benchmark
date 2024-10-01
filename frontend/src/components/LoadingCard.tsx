967ee4d4758d958e64b42eb89a4b749c8f426c16
Dominik Krakau <dominik.krakau@outlook.de>
2024-10-01 06:13:56 +0200

    Adjust loading card

--- frontend/src/components/LoadingCard.tsx
+++ frontend/src/components/LoadingCard.tsx
@@ -2,10 +2,17 @@ import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon } from "
 import { ellipsisHorizontal } from "ionicons/icons";
 import styles from './LoadingCard.module.css';
 
+export enum LoadingState {
+    pending = "Pending",
+    loading = "Loading",
+    received = "Received",
+}
+
 interface LoadingCardProps {
+    dbName: string;
     state: string;
-    query: number;
-    queries: number;
+    queryCount: number;
+    queriesTotal: number;
 }
 
 const LoadingCard: React.FC<LoadingCardProps> = (props) => {
@@ -16,9 +23,13 @@ const LoadingCard: React.FC<LoadingCardProps> = (props) => {
                     <div className={styles.rotate}>
                         <IonIcon className={styles.rotatingImg} src={ellipsisHorizontal} /> {/* sync */}
                     </div>
-                    <p className={styles.loadingTitle}>{props.state}</p>
+                    <p className={styles.loadingTitle}>{props.dbName}</p>
+                    <IonCardSubtitle className={styles.loadingSubtitle}>{props.state}</IonCardSubtitle>
                 </IonCardTitle>
-                <IonCardSubtitle className={styles.loadingSubtitle}>{props.query} / {props.queries}</IonCardSubtitle>
+                {props.state === LoadingState.pending
+                    ? <></>
+                    : <IonCardSubtitle className={styles.loadingSubtitle}>{props.queryCount} / {props.queriesTotal}</IonCardSubtitle>
+                }
             </IonCardHeader>
         </IonCard>
     );
